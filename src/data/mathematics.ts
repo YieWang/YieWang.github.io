5d77a380091f2b43fba5955d426f7ad77a11c6f3
YieWang <Yi.Wang@stud.uni-regensburg.de>
2026-09-12 15:56:09 +0800

    Update LaTeX templates to v=5 with latest revisions

--- src/data/mathematics.ts
+++ src/data/mathematics.ts
@@ -40,19 +40,19 @@ export const templateData: TemplateItem[] = [
   {
     title: "English",
     type: "LaTeX template",
-    previewUrl: "https://homepage-assets.mathtranslations.org/templates/English.pdf?v=4",
-    sourceUrl: "https://homepage-assets.mathtranslations.org/templates/English.zip?v=4",
+    previewUrl: "https://homepage-assets.mathtranslations.org/templates/English.pdf?v=5",
+    sourceUrl: "https://homepage-assets.mathtranslations.org/templates/English.zip?v=5",
   },
   {
     title: "Chinese",
     type: "LaTeX template",
-    previewUrl: "https://homepage-assets.mathtranslations.org/templates/Chinese.pdf?v=4",
-    sourceUrl: "https://homepage-assets.mathtranslations.org/templates/Chinese.zip?v=4",
+    previewUrl: "https://homepage-assets.mathtranslations.org/templates/Chinese.pdf?v=5",
+    sourceUrl: "https://homepage-assets.mathtranslations.org/templates/Chinese.zip?v=5",
   },
   {
     title: "Seminar",
     type: "LaTeX template",
-    previewUrl: "https://homepage-assets.mathtranslations.org/templates/Seminar.pdf?v=4",
-    sourceUrl: "https://homepage-assets.mathtranslations.org/templates/Seminar.zip?v=4",
+    previewUrl: "https://homepage-assets.mathtranslations.org/templates/Seminar.pdf?v=5",
+    sourceUrl: "https://homepage-assets.mathtranslations.org/templates/Seminar.zip?v=5",
   },
 ];
